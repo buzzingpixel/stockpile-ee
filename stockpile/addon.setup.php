4f88de0824f36b44bd180b6779156f8fb74c80b8
TJ Draper <tj@buzzingpixel.com>
2018-01-08 12:47:24 -0600

    Add basic plugin files

---
+++ stockpile/addon.setup.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2018 BuzzingPixel, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+return array(
+    'author' => 'TJ Draper',
+    'author_url' => 'https://buzzingpixel.com',
+    // 'description' => '',
+    'name' => 'Stockpile',
+    'namespace' => 'buzzingpixel\stockpile',
+    'version' => '1.0.0',
+);
---
+++ stockpile/pi.stockpile.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2018 BuzzingPixel, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace buzzingpixel\stockpile;
+
+/**
+ * Class Stockpile
+ */
+class Stockpile
+{
+}
